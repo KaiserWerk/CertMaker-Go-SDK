08c55b24ccf69475f50f7cd7b4746a270b6b8e95
Robin Kaiser <web@r-k.mx>
2021-05-26 21:16:51 +0200

    add function fileExists()

---
+++ helper.go
@@ -0,0 +1,12 @@
+package certmaker
+
+import "os"
+
+// fileExists checks whether a given file exists or not
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
